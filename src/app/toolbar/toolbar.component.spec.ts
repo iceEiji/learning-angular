406e0a2879a42d426186aa688b702cdb5cbdb71b
iceEiji <guitar.pg.sc@gmail.com>
2023-03-25 02:11:25 +0900

    toolbarをコンポーネント切り出し

---
+++ src/app/toolbar/toolbar.component.spec.ts
@@ -0,0 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolbarComponent } from './toolbar.component';
+
+describe('ToolbarComponent', () => {
+  let component: ToolbarComponent;
+  let fixture: ComponentFixture<ToolbarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ToolbarComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ToolbarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(ToolbarComponent);
+    const component = fixture.componentInstance
+    component.title = 'learning-angular';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.toolbar span.title')?.textContent).toContain('learning-angular');
+  });
+});
