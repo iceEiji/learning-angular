0e472ed8243d912b16b3e9ee4e08cb1801ba20d6
iceEiji <guitar.pg.sc@gmail.com>
2023-04-15 03:00:42 +0900

    gzip確認コンポーネントへのルーティングを追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GzipConverterComponent } from './gzip-converter/gzip-converter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'gzip', component: GzipConverterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
