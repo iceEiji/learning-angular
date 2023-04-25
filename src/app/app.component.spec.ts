b0f147e04fe6feb7c2b26431ed29153d45326941
iceEiji <guitar.pg.sc@gmail.com>
2023-04-25 23:52:23 +0900

    refactor: ツールバーとサイドメニューのコンポーネントを分割しないようにした。
    * ツールバーとナビゲーションが別コンポーネントだとレスポンシブ対応がやりづらいため。
    * レスポンシブ対応が終わったら改めて分割する

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { SideNavigationComponent } from './side-navigation/side-navigation.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, ToolbarComponent, SideNavigationComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -22,5 +20,14 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('learning-angular');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    component.title = 'learning-angular-test';
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.toolbar h1.title')?.textContent).toContain('learning-angular');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { GzipConverterComponent } from './gzip-converter/gzip-converter.component';
-import { SideNavigationComponent } from './side-navigation/side-navigation.component';
 import { IntroductionComponent } from './introduction/introduction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
     GzipConverterComponent,
-    SideNavigationComponent,
     IntroductionComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
